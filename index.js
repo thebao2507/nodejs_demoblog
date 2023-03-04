0e0c42e1cabd6fe17425a3a04f64336dc27696b1
thebao <lethebao2507@gmail.com>
2023-03-04 10:46:54 +0700

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3001
 // route - định tuyến
-app.get("/", (req, res) => res.send('hello world'))
+app.get("/", (req, res) => {res.send('hello my world')})
 
 app.listen(port, () => {
     console.log(`example app listening on port ${port}`)
