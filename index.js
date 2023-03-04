81ce1fa23b2afcd206612c8eccac1c3233e590d6
thebao <lethebao2507@gmail.com>
2023-03-04 10:49:43 +0700

    test demo 3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3001
 // route - định tuyến
 app.get("/", (req, res) => {res.send('hello my world')})
-
+//commmm
 app.listen(port, () => {
     console.log(`example app listening on port ${port}`)
 })
